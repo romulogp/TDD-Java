92b7c1abfbf0b444df551535207ed0d2b416da94
Rômulo Göelzer Portolann <romulogoelzer@gmail.com>
2018-08-13 23:49:53 -0300

    Spy e mock, defaults, evitando chamadas indesejadas

--- src/main/java/br/ce/wcaquino/servicos/Calculadora.java
+++ src/main/java/br/ce/wcaquino/servicos/Calculadora.java
@@ -8,19 +8,24 @@ import br.ce.wcaquino.exceptions.NaoPodeDividirPorZeroException;
  */
 public class Calculadora {
 
-    int somar(int a, int b) {
-        return a + b;
-    }
+  int somar(int a, int b) {
+    System.out.println("Somando a + b");
+    return a + b;
+  }
 
-    int subtrair(int a, int b) {
-        return a - b;
-    }
+  int subtrair(int a, int b) {
+    return a - b;
+  }
 
-    double dividir(int a, int b) throws NaoPodeDividirPorZeroException {
-        if (b == 0) {
-            throw new NaoPodeDividirPorZeroException();
-        }
-        return a / b;
+  double dividir(int a, int b) throws NaoPodeDividirPorZeroException {
+    if (b == 0) {
+      throw new NaoPodeDividirPorZeroException();
     }
+    return a / b;
+  }
+  
+  public void imprime() {
+    System.out.println("Passei aqui");
+  }
 
 }
--- src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
@@ -1,15 +1,30 @@
 package br.ce.wcaquino.servicos;
 
 import static org.junit.Assert.assertEquals;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 /**
  *
  * @author Rï¿½mulo Gï¿½elzer Portolann
  */
 public class CalculadoraMockTest {
+
+  @Mock
+  private Calculadora calcMock;
+
+  @Spy
+  private Calculadora calcSpy;
+
+  @Before
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+  }
 
   @Test
   public void teste() {
@@ -29,6 +44,25 @@ public class CalculadoraMockTest {
 
     assertEquals(5, calc.somar(1, 10000));
     System.out.println(argCapt.getAllValues());
+  }
+
+  @Test
+  public void teste3() {
+    Mockito.when(calcMock.somar(1, 2)).thenReturn(3);
+    Mockito.when(calcMock.somar(1, 10)).thenCallRealMethod();
+//    Mockito.when(calcSpy.somar(1, 2)).thenReturn(3);
+    Mockito.doReturn(3).when(calcSpy).somar(1, 2);
+    Mockito.doNothing().when(calcSpy).imprime();
+
+    System.out.println("Mock: " + calcMock.somar(1, 3)); // Mock: quando não sabe o que fazer retorna o valor padrão.
+    System.out.println("Spy: " + calcSpy.somar(1, 3));   // Spy:  quando não sabe o que fazer retorna a execução padrão.
+    // Obs.: O Spy só atua em classes concretas. Portanto, não funcionará com interfaces.
+    System.out.println("Mock chamando metodo. Retorno: " + calcSpy.somar(1, 10));
+
+    System.out.println("Mock");
+    calcMock.imprime(); // o comportamento padrão do MOCK é NÃO EXECUTAR
+    System.out.println("Spy");
+    calcSpy.imprime();  // o comportamento padrão do SPY é EXECUTAR
   }
 
 }
